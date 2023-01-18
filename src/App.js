1607fd20427d39b96c2c64d72e927a384175e92c
islammd39e <islammdrahat308@gmail.comm>
2023-01-18 12:33:23 +0600

    add btn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <button className="btn border-t-neutral-600">click</button>
     </div>
   );
 }
